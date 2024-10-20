65168ae93f2ffe977db3bbf43eeb362203dfaf5b
amritansha82 <amritansha.sinha@kgpian.iitkgp.ac.in>
2024-10-20 23:16:06 +0530

    empty

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <cstdlib>    // for std::getenv
+#include <string>     // for std::string
+#include <sstream>    // for std::stringstream
+#include <unistd.h>   
 using namespace std;
+
+
+std::string get_path(const std::string& command) {
+    // Get the PATH environment variable
+    std::string path_env = std::getenv("PATH");
+    std::stringstream ss(path_env);
+    std::string path;
+
+    // Iterate over each path in the PATH environment variable
+    while (getline(ss, path, ':')) {
+        std::string abs_path = path + "/" + command;
+
+        // Check if the file exists and is executable
+        if (access(abs_path.c_str(), X_OK) == 0) {
+            return abs_path;  // Return the absolute path if found
+        }
+    }
+
+    // Return empty string if command not found
+    return "";
+}
+
 int main() {
 
   std::cout << std::unitbuf;
@@ -14,6 +41,18 @@ int main() {
     if (input == "exit 0") return 0;
     else if (input.find("echo ")==0){
       std::cout << input.substr(5) << std::endl;
+    }
+    else if (input.find("type ")==0){
+      string sub = input.substr(5);
+      if (input.substr(5) == "exit" || input.substr(5) == "echo" || input.substr(5) == "type") std::cout << input.substr(5)<<" "<<"is a shell built-in" << std::endl;
+      else {
+        std::string path = get_path(sub);
+        if (path.empty()) {
+            std::cout << sub << ": not found\n";
+        } else {
+            std::cout << input.substr(5) << " is " << path << std::endl;
+        }
+      }
     }
     else{
       cout << input << ": command not found" << endl;
