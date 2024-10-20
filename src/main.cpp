777fc439551e07e1cd6ef6fe9d32e322540b6ba7
Amritansha Sinha <amritansha.24@kgpian.iitkgp.ac.in>
2024-10-20 13:45:50 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
-
+#include <string>
+using namespace std;
 int main() {
-  // Flush after every std::cout / std:cerr
+
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
-  // Uncomment this block to pass the first stage
-  // std::cout << "$ ";
-  //
-  // std::string input;
+
+  std::cout << "$ ";
+  
+  std::string input;
   // std::getline(std::cin, input);
+  getline(std::cin, input);
 }
